35e4322dadda03a5d5ad651e1b6d24df0961b8ae
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2021-10-14 14:49:11 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,15 @@
 import discord
-Prom discord.ext 
-import commands
+From discord.ext
+ import commands
 
+
+bot = commands.Bot(commands_prefix="f!")
+#定義呼叫機器人的命令字首
+
+@bot.event
+async def on_ready():
+    print(">>bot is online<<")
+
+
+
+bot.run("ODc2NzI5MTc0NTMyOTU2MTYw.YRoTrA.s2yUDQcuPnyNX73PsgulLQ4jTNY")
