39064c1c7752483be23e87f4c3a00e145a38a681
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2021-10-14 18:57:09 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import discord
-From discord.ext import commands
+from discord import channel
+from discord.ext import commands
 
 
-bot = commands.Bot(commands_prefix="f!")
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('f!'))
 #定義呼叫機器人的命令字首
 
 @bot.event
@@ -10,6 +11,13 @@ async def on_ready():
     print(">>bot is online<<")
 
 @bot.event
-async def on_member():
+async def on_member_join(member):
+    channel = bot.get_channel(898159587922935869)
+    await channel.send(f'{member} 歡迎你進來呱呱')
+    
+
+@bot.event
+async def on_member_remove(member):
+    print(f'{member} leave！')
 
 bot.run("ODc2NzI5MTc0NTMyOTU2MTYw.YRoTrA.Wg4eDQwarrPnkh4dj5A6WG5GgJg")
