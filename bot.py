973420b774c6467dd4eb1d513fbde7a28f70cb76
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2021-10-15 17:43:05 +0800

    設json
    讓許多雜亂的字串可以被收納起來

--- bot.py
+++ bot.py
@@ -1,6 +1,11 @@
 import discord
 from discord import channel
 from discord.ext import commands
+import json 
+
+
+with open("setting.json",mode="r",encoding="utf8") as jfile:
+
 
 intents = discord.Intents.all()
 
@@ -11,18 +16,27 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or('f!'),intents = int
 @bot.event
 async def on_ready():
     print(">>bot is online<<")
+#顯示bot在線上
 
 @bot.event
 async def on_member_join(member):
     print("welcome")
     channel = bot.get_channel(898159587922935869)
     await channel.send(f'{member} 歡迎你進來呱呱')
-    
+#歡迎訊息
 
 @bot.event
 async def on_member_remove(member):
     print("byebye")
     channel = bot.get_channel(898159634911752212)
     await channel.send(f'{member} 趕快走吧呱呱')
+#再見訊息
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'{round(bot.latency*1000)}(ms)')
+#對話時機器人的延遲
+
+
 
 bot.run("ODc2NzI5MTc0NTMyOTU2MTYw.YRoTrA.Wg4eDQwarrPnkh4dj5A6WG5GgJg")
