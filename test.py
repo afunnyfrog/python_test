004646eddb296724196ce4c3cb49d91ea80bef8d
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2021-11-18 17:48:50 +0800

    dc嵌入式訊息

--- test.py
+++ test.py
@@ -1,4 +1,9 @@
-import os
-
-for filename in os.listdir("./cmds"):
-    print(filename)
+import time
+import calendar
+from datetime import datetime, time, timedelta, timezone
+dt = datetime.datetime.strptime('2015-07-03 20:25', '%Y-%m-%d %H:%M').replace(tzinfo=us)
+time.mktime(dt.utctimetuple())
+# 1435955100 => 2015-07-03 20:25 GMT
+calendar.timegm(dt.utctimetuple())
+# 1435983900 => 2015-07-04 04:25 GMT
+print(datetime.timestamp())
