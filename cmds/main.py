7bc97cb6c7477769a93146e7b62e9c1034a2a411
afunnyfrog <90972974+afunnyfrog@users.noreply.github.com>
2021-12-16 20:55:27 +0800

    多工處理&異步工作

--- cmds/main.py
+++ cmds/main.py
@@ -8,7 +8,6 @@ from datetime import datetime, timedelta, timezone
 utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
 time =utc_dt.strftime("%Y-%m-%d %H:%M:%S")
 class Main(Cog_Extension):
-
     @commands.command()
     async def ping(self,ctx):
         await ctx.send(f'{round(self.bot.latency*1000)}(ms)')
---
+++ cmds/task.py
@@ -0,0 +1,23 @@
+import discord
+from discord import channel
+from discord.ext import commands
+from core.classes import Cog_Extension
+import random
+import json,asyncio,datetime
+
+class Task(Cog_Extension):
+    def __init__(self,*args,**kwargs):
+        super().__init_(*args,**kwargs)
+        
+        async def interval():
+            await self.bot.wait_untial_ready()
+            self.channel = self.bot.get_channel(920930478729596928) 
+            while not self.bot.is_cloesd():
+                await self.channel.send('呱，我在')
+                await asyncio.sleep(5)#單位：秒
+
+        self.bg_task=self.bot.loop.creat_task(interval())
+
+def setup(bot):
+    bot.add_cog(Task(bot))
+  
